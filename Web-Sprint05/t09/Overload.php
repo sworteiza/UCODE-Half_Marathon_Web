352206449dddfa5d805640f735b52d5ea3d22b06
SWORTOI <51696583+sworteiza@users.noreply.github.com>
2021-03-25 14:19:36 +0200

    Add files via upload

--- Web-Sprint05/t09/Overload.php
+++ Web-Sprint05/t09/Overload.php
@@ -1,3 +1,36 @@
 <?php
+    class Overload{
+        public $data = [];
+        function __get($id){
+            if(array_key_exists($id, $this->data)){
+                return $this->data[$id];
+            }
+            else{
+                $str = "NO DATA";
+                return $str;
+            }
+        }
+        function __set($id, $val){
+            $this->data[$id] = $val;
+        }
+        function __isset($id){
+            if(array_key_exists($id, $this->data)){
+                return isset($this->data[$id]);
+            }
+            else{
+                $this->data[$id] = "NOT SET";
+                return $this->data[$id];
+            }
+        }
+        function __unset($id){
+            if(array_key_exists($id, $this->data)){
+                unset($this->data[$id]);
+            }
+            else{
+                $this->data[$id] = NULL;
+            }
+        }
+    }
+
 
 ?>
